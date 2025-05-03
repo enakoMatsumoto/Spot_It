c84a36cfbc3debb73c1005f25e00a05b7fdfbb06
Paul Yang <paulyang0709@gmail.com>
2025-05-03 16:53:23 -0400

    update path and add initfile for tests

--- tests/test_grpc.py
+++ tests/test_grpc.py
@@ -5,6 +5,8 @@ import threading
 import os
 import sys
 
+# Add the parent directory to the path so we can import the modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import chat_pb2
 import chat_pb2_grpc
 
--- tests/test_spotit.py
+++ tests/test_spotit.py
@@ -1,4 +1,9 @@
 import unittest
+import sys
+import os
+
+# Add the parent directory to the path so we can import the modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from spotit_game_logic import SpotItGame, generate_cards, shuffle_cards
 from collections import deque
 
